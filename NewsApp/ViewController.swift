2fda720540305170b346712003fecaaec2d09231
Danil Ryumin <ryumin_danil@icloud.com>
2023-02-16 19:02:09 +0300

    Create newsTableView

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  NewsApp
-//
-//  Created by Danil Ryumin on 15.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var newsTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newsTableView.delegate = self
+        newsTableView.dataSource = self
+        view.addSubview(newsTableView)
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        newsTableView.frame = view.bounds
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         return UITableViewCell()
+    }
+}
