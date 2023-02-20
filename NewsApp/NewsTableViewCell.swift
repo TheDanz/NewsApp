2cc811d547a6133f4aa70fa2f2b966e0eb2935ae
Danil Ryumin <ryumin_danil@icloud.com>
2023-02-20 22:11:15 +0300

    Create NewsTableViewCell

---
+++ NewsApp/NewsTableViewCell.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let cellIdentifier = "NewsTableViewCell"
+    var data: [Article] = []
+    let apiCaller = APICaller()
+    
     var newsTableView: UITableView = {
         let tableView = UITableView()
         return tableView
@@ -13,6 +17,16 @@ class ViewController: UIViewController {
         newsTableView.delegate = self
         newsTableView.dataSource = self
         view.addSubview(newsTableView)
+        
+        let xibCell = UINib(nibName: cellIdentifier, bundle: nil)
+        newsTableView.register(xibCell, forCellReuseIdentifier: cellIdentifier)
+        
+        apiCaller.dataRequest { articles in
+            self.data = articles
+            DispatchQueue.main.async {
+                self.newsTableView.reloadData()
+            }
+        }
     }
     
     override func viewDidLayoutSubviews() {
@@ -23,16 +37,28 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? NewsTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.titleLabel.text = data[indexPath.row].title
+        let url = URL(string: data[indexPath.row].urlToImage!)
+        apiCaller.getPoster(from: url!) { image in
+            DispatchQueue.main.async {
+                cell.posterImageView.image = image
+            }
+        }
+        return cell
     }
 }
