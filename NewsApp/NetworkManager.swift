af7df8e2296efeda4641da2fd2cc570f9001ba7f
Danil Ryumin <ryumin_danil@icloud.com>
2023-03-06 01:51:45 +0300

    Update NetworkManager

--- NewsApp/NetworkManager.swift
+++ NewsApp/NetworkManager.swift
@@ -3,23 +3,19 @@ import Foundation
 
 class NetworkManager {
     
-    let APIKey = "8be4ff77f9294f22a4ab354b89de632c"
-    let session = URLSession.shared
+    private let APIKey = "8be4ff77f9294f22a4ab354b89de632c"
+    private let session = URLSession.shared
     
     func getArticles(query: String, completion: @escaping (Result<[Article], Error>) -> Void) {
         
-        guard let URL = URL(string: "https://newsapi.org/v2/everything?q=\(query)&language=en&pageSize=20&apiKey=\(APIKey)") else {
-            return
-        }
+        guard let url = getURL(query: query) else { return }
         
-        let task = session.dataTask(with: URL) { data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
             
             guard let data = data,
                   let response = response as? HTTPURLResponse,
                   (200...299).contains(response.statusCode),
-                  error == nil else {
-                return
-            }
+                  error == nil else { return }
             
             if let articles = self.parseJSON(dataToDecode: data) {
                 DispatchQueue.main.async {
@@ -52,5 +48,17 @@ class NetworkManager {
         let newsList = try? parser.decode(NewsList.self, from: dataToDecode)
         let articles = newsList?.articles
         return articles
+    }
+    
+    private func getURL(query: String) -> URL? {
+        
+        guard var urlComponent = URLComponents(string: "https://newsapi.org") else { return nil }
+        urlComponent.path = "/v2/everything"
+        let queryURLQueryItem = URLQueryItem(name: "q", value: query)
+        let languageURLQueryItem = URLQueryItem(name: "language", value: "en")
+        let apiKeyURLQueryItem = URLQueryItem(name: "apiKey", value: APIKey)
+        urlComponent.queryItems = [queryURLQueryItem, languageURLQueryItem, apiKeyURLQueryItem]
+        guard let url = urlComponent.url else { return nil }
+        return url
     }
 }
